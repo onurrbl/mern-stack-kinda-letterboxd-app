c9c8e712dd961ddfd9a33fc7c167b90b7d51ccb0
onurrbl <70076850+onurrbl@users.noreply.github.com>
2022-08-02 21:19:41 +0300

    configure webpack and initialize react project setup in client

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>Hell0 World</div>
+  )
+}
+
+export default App
---
+++ client/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+module.exports = {
+  mode: "development",
+  entry: { bundle: path.resolve(__dirname, "src/index.js") },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devtool: 'source-map',
+  devServer : {
+    static: {
+        directory: path.resolve(__dirname, 'build')
+    },
+    port:3000,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+        }
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+}
